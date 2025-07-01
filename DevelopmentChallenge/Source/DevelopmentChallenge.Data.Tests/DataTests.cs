a84a6040462ae16bd104bb535e5b801b00679556
Marcelo Silva <marc.oasiss@gmail.com>
2025-07-01 12:01:49 -0300

    Ajustes em mensagens de validação de testes unitários

--- DevelopmentChallenge/Source/DevelopmentChallenge.Data.Tests/DataTests.cs
+++ DevelopmentChallenge/Source/DevelopmentChallenge.Data.Tests/DataTests.cs
@@ -37,7 +37,7 @@ namespace DevelopmentChallenge.Data.Tests
 
             var resumen = ReporteFormas.Imprimir(cuadrados, SupportedLanguage.Castellano);
 
-            Assert.AreEqual("<h1>Reporte de Formas</h1>1 Cuadrado | Area 25 | Perimetro 20 <br/><br/>TOTAL:<br/>1 formas Perimetro 20 Area 25", resumen);
+            Assert.AreEqual("<h1>Reporte de Formas</h1>1 Cuadrado | Area 25 | Perimetro 20<br/><br/>TOTAL:<br/>1 formas Perimetro 20 Area 25", resumen);
         }
 
         [Test]
@@ -52,7 +52,7 @@ namespace DevelopmentChallenge.Data.Tests
 
             var resumen = ReporteFormas.Imprimir(cuadrados, SupportedLanguage.Ingles);
 
-            Assert.AreEqual("<h1>Shapes report</h1>3 Squares | Area 35 | Perimeter 36 <br/><br/>TOTAL:<br/>3 shapes Perimeter 36 Area 35", resumen);
+            Assert.AreEqual("<h1>Shapes report</h1>3 Squares | Area 35 | Perimeter 36<br/><br/>TOTAL:<br/>3 shapes Perimeter 36 Area 35", resumen);
         }
 
         [Test]
@@ -72,10 +72,10 @@ namespace DevelopmentChallenge.Data.Tests
             var resumen = ReporteFormas.Imprimir(formas, SupportedLanguage.Ingles);
 
             Assert.AreEqual(
-                "<h1>Shapes report</h1>2 Squares | Area 29 | Perimeter 28 <br/>" +
-                "2 Circles | Area 13.01 | Perimeter 18.06<br/>" +
-                "3 Triangles | Area 49.64 | Perimeter 51.6<br/><br/>" +
-                "TOTAL:<br/>7 shapes Perimeter 97.66 Area 91.65", resumen);
+                "<h1>Shapes report</h1>2 Squares | Area 29 | Perimeter 28<br/>" +
+                "2 Circles | Area 13,01 | Perimeter 18,06<br/>" +
+                "3 Triangles | Area 49,64 | Perimeter 51,6<br/><br/>" +
+                "TOTAL:<br/>7 shapes Perimeter 97,66 Area 91,65", resumen);
         }
 
         [Test]
@@ -95,10 +95,10 @@ namespace DevelopmentChallenge.Data.Tests
             var resumen = ReporteFormas.Imprimir(formas, SupportedLanguage.Castellano);
 
             Assert.AreEqual(
-                "<h1>Reporte de Formas</h1>2 Cuadrados | Area 29 | Perimetro 28 <br/>" +
-                "2 Círculos | Area 13.01 | Perimetro 18.06<br/>" +
-                "3 Triángulos | Area 49.64 | Perimetro 51.6<br/><br/>" +
-                "TOTAL:<br/>7 formas Perimetro 97.66 Area 91.65", resumen);
+                "<h1>Reporte de Formas</h1>2 Cuadrados | Area 29 | Perimetro 28<br/>" +
+                "2 Círculos | Area 13,01 | Perimetro 18,06<br/>" +
+                "3 Triángulos | Area 49,64 | Perimetro 51,6<br/><br/>" +
+                "TOTAL:<br/>7 formas Perimetro 97,66 Area 91,65", resumen);
         }
     }
 }
